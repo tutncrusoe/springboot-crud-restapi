aaa466ec09cc8e2bc3404ea410d0b52649fac9d8
Thai Pham <thai.pham@rtrobotics.com>
2022-12-16 00:11:35 +0700

    create codebase

---
+++ src/main/java/com/example/springbootcrudrestapi/SpringbootCrudRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestapiApplication.class, args);
+	}
+
+}
